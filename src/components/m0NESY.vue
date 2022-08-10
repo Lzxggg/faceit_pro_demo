c432bee8eef059d3b3edb9b04284f0713806970c
XiaoLei <44516533+Lzxggg@users.noreply.github.com>
2022-08-10 15:54:30 +0800

    Update m0NESY.vue

--- src/components/m0NESY.vue
+++ src/components/m0NESY.vue
@@ -19,7 +19,7 @@
    headers: {//指定响应头
        "Content-Type": "application/json;charset=utf-8",
        "Accept": "application/json",
-       authorization:`Bearer 492c776c-ec76-4cf4-8dcc-cc6cab2822f6`
+       authorization:`Bearer `
    }
     }
     // const Faceit = require('faceit-js-api')
